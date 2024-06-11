5c18f77042f76fe444938b8a1a3e0810f8f16358
Kamille Magdirila <kamillenmagdirila@gmail.com>
2024-06-11 08:42:07 -0700

    part 1

---
+++ part-1/FirstComponent.js
@@ -0,0 +1,8 @@
+
+const FirstComponent = (props) => {
+    return(
+        <div>
+            <h1> My First Component </h1>
+        </div>
+    )
+}
---
+++ part-1/NamedComponent.js
@@ -0,0 +1,8 @@
+
+const NamedComponent = (props) => {
+    return(
+        <div>
+            <p> My name is {props.name} </p>
+        </div>
+    )
+}
